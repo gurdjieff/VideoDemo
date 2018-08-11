79f90c1ebfb991e2ee6ac852cffe72d8bb65265c
daiyu zhang <gurdjieff100@gmail.com>
2018-08-11 12:21:34 -0700

    Initial Commit

---
+++ TubiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TubiDemo
+//
+//  Created by daiyu zhang on 8/11/18.
+//  Copyright © 2018 daiyu zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
